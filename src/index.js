2ee3e1f0084ce021a8ab9eedb909fdaa62851229
Eric Maccoux <43770992+ECMaccoux@users.noreply.github.com>
2021-05-21 16:58:36 -0500

    Base Firebase setup

--- src/index.js
+++ src/index.js
@@ -4,6 +4,29 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import firebase from "firebase/app";
+
+// If you enabled Analytics in your project, add the Firebase SDK for Analytics
+import "firebase/analytics";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAA0-FxovQP0mfokwXUeAmBg_8CsCdtHwc",
+  authDomain: "maccoux-mxdb.firebaseapp.com",
+  projectId: "maccoux-mxdb",
+  storageBucket: "maccoux-mxdb.appspot.com",
+  messagingSenderId: "545552041812",
+  appId: "1:545552041812:web:6fba62750da9923ade6ac1",
+  measurementId: "G-EFRDX1X9DG"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
